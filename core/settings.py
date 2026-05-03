9ecf76c1d59ab3b8e7759227d62dec8edc2e1ae8
Rodrigo de sousa barbosa <detonadorrodrigo11@gmail.com>
2026-05-03 14:53:19 -0300

    Segurança: Removida SECRET_KEY hardcoded do settings.py

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '8139288492152007')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
